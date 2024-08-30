b6aef28996d7494e7c7eae507a51385db32c4653
Dfacundorod <112908434+Dfacundorod@users.noreply.github.com>
2024-08-30 00:41:06 +0000

    add bonus task

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -5,6 +5,8 @@ export const TrafficLight = ()=>{
     const [red, setRed] = useState('')
     const [yellow, setYellow] = useState('')
     const [green, setGreen] = useState('')
+    const [violet, setViolet] = useState('none')
+    let random =1;
     function shadowRed(){
         if(red==''){
             setRed('shadowRed')
@@ -32,11 +34,45 @@ export const TrafficLight = ()=>{
             setGreen('')
         }
     }
+
+    function initRandom(){
+        setInterval(()=>{
+           if(random===1){
+            setGreen('shadowGreen')
+            setYellow('')
+            setRed('')
+            random=2
+           } 
+           else if(random===2){
+            setYellow('shadowYellow')
+            setRed('')
+            setGreen('')
+            random=3
+           }
+            else if(random===3){
+            setRed('shadowRed')
+            setYellow('')
+            setGreen('')
+            random =1
+        }
+        },1000)
+    }
+   function lightViolet(){
+    if(violet==='none'){
+        alert(`Se agregara violeta`)
+        setViolet('flex')
+    } else{setViolet(`none`)}
+   }
     return (
-        <div className="container col-2 bg-black d-flex flex-column justify-content-center align-items-stretch  trafficLight">
-            <button className={`btn btn-danger rounded-circle p-3 lh-1 my-1 ${red}`} style={{height: `160px`}} onClick={shadowRed}></button>
-            <button className={`btn btn-warning rounded-circle p-3 lh-1 my-1 ${yellow}`} style={{height: `160px`}} onClick={lightYellow}></button>
-            <button className={`btn btn-success rounded-circle p-3 lh-1 my-1 ${green}`} style={{height: `160px`}} onClick={lightGreen}></button>
-        </div>
+        <>
+            <button className="col-2 btn btnRandom" onClick={initRandom}>Random</button>
+            <button className="col-2 btn btnViolet"  onClick={lightViolet}>Violet</button>
+            <div className="container col-2 bg-black d-flex flex-column justify-content-center align-items-stretch  trafficLight" style={{width:`170px`}}>
+                <button className={`btn btn-danger rounded-circle p-3 lh-1 my-1 ${red}`} style={{height: `160px`}} onClick={shadowRed}></button>
+                <button className={`btn btn-warning rounded-circle p-3 lh-1 my-1 ${yellow}`} style={{height: `160px`}} onClick={lightYellow}></button>
+                <button className={`btn btn-success rounded-circle p-3 lh-1 my-1 ${green}`} style={{height: `160px`}} onClick={lightGreen}></button>
+                <button className={`btn rounded-circle p-3 lh-1 my-1 `} style={{height: `160px`, display: `${violet}`, backgroundColor: `violet`}}></button>
+            </div>
+        </>
     )
 }
